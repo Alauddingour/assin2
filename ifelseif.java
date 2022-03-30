a2abbf96d2ab9b8a3a772f4e31a7538ec5369ca1
Alauddingour <101685829+Alauddingour@users.noreply.github.com>
2022-03-30 17:58:59 +0530

    Add files via upload

---
+++ ifelseif.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class ifelseif 
+{
+
+	public static void main(String[] args) 
+	{
+
+		int a=10;
+// in case if we want to test multiple conditions
+// when first condition is true then control will come out of if 
+		
+	if(a<5)  
+		{
+			 System.out.println("hello");
+			 System.out.println("hello 2");
+		}
+		else if(a==40)	{
+	 		            System.out.println("welcome");
+	      	            }
+		else if(a<2)System.out.println("testing ");
+		else
+			 System.out.println("sorry");
+	}
+
+}
